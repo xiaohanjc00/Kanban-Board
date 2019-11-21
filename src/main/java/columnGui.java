2aebbfb725c5e7f6abd6803e15042585b1d4136e
xiaohanjc00 <xiaohanjc00@gmail.com>
2019-11-21 16:00:52 +0000

    Update columnGui.java

--- src/main/java/columnGui.java
+++ src/main/java/columnGui.java
@@ -2,29 +2,58 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.*;
 
-public class columnGui {
+public class ColumnGui {
 
-    private ArrayList<Card> cards;
     private String name;
-
-    public columnGui(ArrayList<Cards> cards, String name) {
-        this.cards = cards;
+    private Column column;
+    private JPanel panel;
+   
+    public ColumnGui(String name) {
         this.name = name;
+        column = new Column(name,name);
+        generatePanel();
     }
 
     public JPanel generatePanel() {
-
-        JPanel panel = new JPanel();
+        panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         JLabel nameLabel = new JLabel(name);
         panel.add(nameLabel);
         
-        for (int i = 0; i < cards.size(); i++) {
-            JButton btn = new JButton(cards[i].getTitle());
-            panel.add(btn);
-
-        }
+        createCards(panel);
 
         return panel;
+    }
+
+    /*
+    Create Buttons for each card
+    */
+    public void createCards(JPanel panel){
+        ArrayList<CardGui> arrayOfCards = column.getCards();
+        for (int i = 0; i < column.getCards().size(); i++) {
+            CardGui newCard = arrayOfCards.get(i);
+            panel.add(newCard);
+        }
+    }
+    /*
+    Create new cards for the column
+    */
+    public void addCard(CardGui newCard){
+        column.addCard(newCard);
+        panel.add(newCard);
+    }
+
+    /*
+    Delete cards from the column
+    */
+    public void deleteCard(String cardName){
+
+    }
+
+    /*
+    Edit cards from the column
+    */
+    public void editCard(String cardName){
+
     }
 }
