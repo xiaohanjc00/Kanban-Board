8c0178788ce726e5e8ca5ccbecee6a29c8a4d326
AlanoThe1o <32736654+AlanoThe1o@users.noreply.github.com>
2019-12-02 17:59:10 +0000

    Linked GUI to member backlog

--- src/main/java/MemberAddGUI.java
+++ src/main/java/MemberAddGUI.java
@@ -1,11 +1,11 @@
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JButton;
-import javax.swing.JTextField;
-import javax.swing.JTextArea;
-import javax.swing.JLabel;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class MemberAddGUI extends JFrame{
+public class MemberAddGUI extends JFrame implements ActionListener{
+    /*
+    Creation of panel, text fields and submit button
+     */
     JPanel p = new JPanel();
     JButton submit = new JButton("Add Member");
     JTextField usernameField = new JTextField("Enter Username");
@@ -16,17 +16,36 @@ public class MemberAddGUI extends JFrame{
         new MemberAddGUI();
     }
 
+    /*
+    GUI set up in the constructor
+     */
     public MemberAddGUI(){
         super("Add Member");
         setSize(400,300);
         setResizable(true);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-
+        submit.addActionListener(this);
         p.add(usernameField);
         p.add(firstNameField);
         p.add(secondNameField);
         p.add(submit);
         add(p);
+
     }
+
+    /*
+    Adds functionality for the submit button in the GUI, takes the text in the text fields and creates a new member.
+     */
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource() == submit){
+            String username = usernameField.getText();
+            String firstName = firstNameField.getText();
+            String secondName = secondNameField.getText();
+            Members newMember = new Members(username,firstName,secondName);
+            JOptionPane.showMessageDialog(null, newMember.getUsername() + " created!");
+        }
+    }
+
+
  }
